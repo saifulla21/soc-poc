83a6de1b31335aa9390bbdc32c7084ee5f744bff
Saifulla Shaik <shaiksaifulla21@gmail.com>
2018-05-03 16:59:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.config['DEBUG'] = False
+socketio = SocketIO(app,engineio_logger=False,log_output=False,async_mode='eventlet')
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
-@app.route('/count')
-def hello_count():
-  return 'Hello, Count!'
+def index():
+    return 'hello World !!'
+    
+@app.route('/test')
+def test():
+    return 'test'
+
+@socketio.on('myevent')
+def test_message(message):
+    emit('myresponse', {'data': 'got it!'})
+    
+@socketio.on('chat message')
+def test_message(message):
+    emit('chat message', message)
 
 if __name__ == '__main__':
-  app.run()
+    socketio.run(app)
