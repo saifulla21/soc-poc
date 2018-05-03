e1dc5c4cb0f8341f02ceb67214708b6c16b343c9
Saifulla Shaik <shaiksaifulla21@gmail.com>
2018-05-03 15:52:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+@app.route('/count')
+def hello_count():
+  return 'Hello, Count!'
 
 if __name__ == '__main__':
   app.run()
